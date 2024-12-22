5e8a4378d58b938542652b99c5aeea428b77485e
Harsh3369 <harshitsundriyal@gmail.com>
2024-12-22 23:53:19 +0530

    created vector database

--- 05_RAG/01_rag_basics.py
+++ 05_RAG/01_rag_basics.py
@@ -5,6 +5,9 @@ from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma, FAISS
 from langchain_openai import OpenAIEmbeddings
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Define the directory containing the text file and the persistent directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "content_for_rag", "google_ai_engineer_notes.txt")
@@ -28,4 +31,22 @@ if not os.path.exists(persistent_directory):
     #Split the documents into chunks
     text_splitter = CharacterTextSplitter(chunk_size = 1000, chunk_overlap=0)
     docs = text_splitter.split_documents(documents)
-    print(len(docs))
+
+    #Display informations about the the split documents
+    print(f"Number of documents: {len(docs)}")
+    print(f"Sample document: {docs[0].page_content}")
+
+    # Create embeddings
+    print("\n--- Creating embeddings ---")
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-small"
+    )  # Update to a valid embedding model if needed
+    print("\n--- Finished creating embeddings ---")
+
+    # Create the vector store and persist it automatically using CPU as device
+    print("\n--- Creating vector store ---")
+    db = Chroma.from_documents(
+        docs, embeddings, persist_directory=persistent_directory)
+    print("\n--- Finished creating vector store ---")
+else:
+    print("Vector store already exists. No need to initialize.")
